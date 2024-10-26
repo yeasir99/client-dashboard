b1fbd417938a95a73aea1c45501ee7c0c3549d27
Yeasir Ahamed <yahamed007@gmail.com>
2024-10-26 18:13:47 +0600

    fix a typo

--- src/app/dashboard/book-management/page.js
+++ src/app/dashboard/book-management/page.js
@@ -4,7 +4,7 @@ import BookManagement from '@/components/dashboard/BookManagement';
 const page = () => {
   return (
     <div>
-      <h1 className="text-2xl capitalize mb-3">book-Category management</h1>
+      <h1 className="text-2xl capitalize mb-3">book management</h1>
       <div className="flex justify-between items-center">
         <Link href="/dashboard/book-management/add">
           <button className="capitalize bg-primary px-2 py-1 text-white rounded-md">
