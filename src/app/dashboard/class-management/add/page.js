00940d9575baad055911320e1333b21f1e5b4c81
Yeasir Ahamed <yahamed007@gmail.com>
2024-11-07 22:35:52 +0600

    update class info

--- src/app/dashboard/class-management/add/page.js
+++ src/app/dashboard/class-management/add/page.js
@@ -11,7 +11,7 @@ const page = () => {
     const res = await axios.post(
       'https://kblsf.site/DLogicKBL/salesforce_api.php?action=create_classinfo',
       {
-        CategoryType: 'books-category',
+        CategoryType: 'ClassInfo',
         CategoryName: formData.classManagement,
       }
     );
