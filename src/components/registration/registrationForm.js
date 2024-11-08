a8d6c48815caf169e6bf7261e70d495d72894885
Yeasir Ahamed <yahamed007@gmail.com>
2024-11-08 22:11:30 +0600

    update reg

--- src/components/registration/registrationForm.js
+++ src/components/registration/registrationForm.js
@@ -1,36 +1,112 @@
+'use client';
+import { useState, useEffect } from 'react';
+import { RxAvatar } from 'react-icons/rx';
+import axios from 'axios';
+
 const registrationForm = () => {
+  const [desigs, setDesigs] = useState([]);
+  const [reportingTo, setReportingTo] = useState([]);
+
+  const getDesig = async cb => {
+    const res = await axios.get(
+      'https://kblsf.site/DLogicKBL/salesforce_api.php?action=get_desigs'
+    );
+    if (res.status === 200) {
+      cb(res.data);
+    }
+  };
+
+  const getReporting = async cb => {
+    const res = await axios.get(
+      'https://kblsf.site/DLogicKBL/salesforce_api.php?action=get_reporttoUsers'
+    );
+    if (res.status === 200) {
+      cb(res.data);
+    }
+  };
+
+  useEffect(() => {
+    getDesig(setDesigs);
+    getReporting(setReportingTo);
+  }, []);
+  const [formData, setFormData] = useState({
+    image: '',
+    employeeId: '',
+    employeeName: '',
+    dasignationRole: '',
+    userId: '',
+    password: '',
+    email: '',
+    phone: '',
+    address: '',
+    reportingTo: '',
+    status: '',
+  });
+  console.log(formData);
+
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
   return (
-    <form className="w-full">
+    <form
+      className="w-full"
+      action="/api/signup"
+      method="POST"
+      encType="multipart/form-data"
+    >
       <div>{/* image upload here */}</div>
       <div className="mb-5">
         <div>
+          <div className="flex justify-center">
+            <label
+              htmlFor="Image"
+              className="capitalize flex font-semibold text-md py-1"
+            >
+              <RxAvatar className="text-8xl" />
+            </label>
+          </div>
+
+          <input
+            id="Image"
+            name="image"
+            type="file"
+            className="w-full rounded-md mb-1 invisible"
+            onChange={e => {
+              const file = e.target.files[0];
+              setFormData({ ...formData, [e.target.name]: file });
+            }}
+          />
+        </div>
+        <div>
           <label
-            htmlFor="employeeID"
+            htmlFor="EmployeeID"
             className="capitalize flex font-semibold text-md py-1"
           >
             employeeID:
           </label>
 
           <input
-            id="employeeID"
-            name="employeeID"
+            id="EmployeeID"
+            name="employeeId"
             type="text"
             className="w-full rounded-md mb-1"
+            onChange={handleChange}
           />
         </div>
         <div>
           <label
-            htmlFor="employeeName"
+            htmlFor="EmployeeName"
             className="capitalize flex font-semibold text-md py-1"
           >
             employee Name:
           </label>
 
           <input
-            id="employeeName"
+            id="EmployeeName"
             type="text"
             name="employeeName"
             className="w-full rounded-md mb-1"
+            onChange={handleChange}
           />
         </div>
         <div>
@@ -38,28 +114,35 @@ const registrationForm = () => {
             Dasignation/Role:
           </label>
 
-          <select name="zone" className="w-full rounded-md">
-            <option value="" disabled={true} selected>
-              NSM
-            </option>
-            <option value="Zone one">Zone one</option>
-            <option value="Zone two">Zone two</option>
-            <option value="Zone three">Zone three</option>
+          <select
+            name="dasignationRole"
+            className="w-full rounded-md"
+            defaultValue=""
+            onChange={handleChange}
+          >
+            <option value="" disabled={true} selected></option>
+            {desigs.length &&
+              desigs.map(item => (
+                <option value={item.ID} key={item.ID}>
+                  {item.CategoryName}
+                </option>
+              ))}
           </select>
         </div>
         <div>
           <label
-            htmlFor="userID"
+            htmlFor="UserID"
             className="capitalize flex font-semibold text-md py-1"
           >
             user ID:
           </label>
 
           <input
-            id="userID"
+            id="UserID"
             type="text"
-            name="userID"
+            name="userId"
             className="w-full rounded-md mb-1"
+            onChange={handleChange}
           />
         </div>
         <div>
@@ -75,69 +158,74 @@ const registrationForm = () => {
             type="text"
             name="userName"
             className="w-full rounded-md mb-1"
+            onChange={handleChange}
           />
         </div>
 
         <div>
           <label
-            htmlFor="password"
+            htmlFor="Password"
             className="capitalize flex font-semibold text-md py-1"
           >
             password:
           </label>
 
           <input
-            id="password"
+            id="Password"
             type="password"
             name="password"
             className="w-full rounded-md mb-1"
+            onChange={handleChange}
           />
         </div>
 
         <div>
           <label
-            htmlFor="email"
+            htmlFor="Email"
             className="capitalize flex font-semibold text-md py-1"
           >
             email:
           </label>
 
           <input
-            id="email"
+            id="Email"
             type="email"
             name="email"
             className="w-full rounded-md mb-1"
+            onChange={handleChange}
           />
         </div>
 
         <div>
           <label
-            htmlFor="phone"
+            htmlFor="Phone"
             className="capitalize flex font-semibold text-md py-1"
           >
             Phone:
           </label>
 
           <input
-            id="phone"
+            id="Phone"
             type="number"
             name="phone"
             className="w-full rounded-md mb-1"
+            onChange={handleChange}
           />
         </div>
         <div>
           <label
-            htmlFor="address"
+            htmlFor="Address"
             className="capitalize flex font-semibold text-md py-1"
           >
             address:
           </label>
 
           <input
-            id="address"
+            id="Address"
             type="text"
             name="address"
             className="w-full rounded-md mb-1"
+            onChange={handleChange}
           />
         </div>
         <div>
@@ -145,13 +233,35 @@ const registrationForm = () => {
             reporting to:
           </label>
 
-          <select name="district" className="w-full rounded-md">
-            <option value="" disabled={true} selected>
-              John Smith -CEO
-            </option>
-            <option value="District one">District one</option>
-            <option value="District two">District two</option>
-            <option value="District three">District three</option>
+          <select
+            name="reportingTo"
+            className="w-full rounded-md"
+            defaultValue=""
+            onChange={handleChange}
+          >
+            <option value="" disabled={true} selected></option>
+            {reportingTo.length &&
+              reportingTo.map(item => (
+                <option value={item.UserID} key={item.UserID}>
+                  {item.reportUsers}
+                </option>
+              ))}
+          </select>
+        </div>
+        <div>
+          <label className="capitalize flex font-semibold text-md py-1">
+            Status:
+          </label>
+
+          <select
+            name="status"
+            className="w-full rounded-md"
+            defaultValue=""
+            onChange={handleChange}
+          >
+            <option value="" disabled={true} selected></option>
+            <option value="true">True</option>
+            <option value="false">False</option>
           </select>
         </div>
       </div>
