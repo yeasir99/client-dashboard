c6a035f08179d60ed65768f02775a0478350a1fb
Yeasir Ahamed <yahamed007@gmail.com>
2024-11-12 02:08:13 +0600

    add image

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,17 @@
 import SarinaFontWrapper from '../sarinaF/sarinaFontWrapper';
 import NavItem from './NavItem';
+import Image from 'next/image';
 const Navbar = () => {
   return (
     <div>
       <div>
-        <div className="bg-primary ">
-          <SarinaFontWrapper>
-            <h1 className="text-surface1 text-xl text-center py-2">
-              D - Logic Sale Force Application
-            </h1>
-          </SarinaFontWrapper>
+        <div className="bg-primary flex justify-center">
+          <Image
+            src="/images/anupom.jpg"
+            height={20}
+            width={118}
+            alt="Brand Name"
+          />
         </div>
         <NavItem />
       </div>
