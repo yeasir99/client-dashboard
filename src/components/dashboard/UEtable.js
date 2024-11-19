fbb734c6470715193b7990db85870fdf0b669c4d
Yeasir Ahamed <yahamed007@gmail.com>
2024-11-19 22:44:21 +0600

    update active and deactive button work

--- src/components/dashboard/UEtable.js
+++ src/components/dashboard/UEtable.js
@@ -1,6 +1,7 @@
 'use client';
 import { FaEye, FaRegEdit } from 'react-icons/fa';
 import useGetData from '@/utils/useGetData';
+import axios from 'axios';
 
 const UEtable = () => {
   const url =
@@ -12,6 +13,24 @@ const UEtable = () => {
   if (status === 'error') {
     return <div>something went wrong</div>;
   }
+
+  const handleDeactive = async item => {
+    const url = `https://kblsf.site/DLogicKBL/salesforce_api.php?action=update_sndUserStatus&UserID=${item.UserID}`;
+    const res = await axios.put(url, { Status: false });
+    if (res.status === 200) {
+      window.location.reload();
+    }
+  };
+
+  const handleActive = async item => {
+    const url = `https://kblsf.site/DLogicKBL/salesforce_api.php?action=update_sndUserStatus&UserID=${item.UserID}`;
+    const res = await axios.put(url, { Status: true });
+    if (res.status === 200) {
+      window.location.reload();
+    }
+  };
+
+  console.log(data);
 
   return (
     <div className="flex flex-col">
@@ -104,23 +123,43 @@ const UEtable = () => {
                     <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 dark:border-white/10">
                       {item.ReportingToUserID}
                     </td>
-                    <td className="whitespace-nowrap px-6 py-4 flex justify-center gap-3">
+                    <td className="whitespace-nowrap px-6 py-4 flex justify-center items-center gap-3">
                       <div>
                         <span className="bg-cyan-500 p-1 inline-block rounded-md">
                           <FaEye className="text-white text-sm" />
                         </span>
-                        <p className="text-sm text-green-600 font-semibold">
-                          Active
-                        </p>
                       </div>
                       |
                       <div>
                         <span className="bg-amber-600 p-1 inline-block rounded-md">
                           <FaRegEdit className="text-white text-sm" />
                         </span>
-                        <p className="text-sm text-red-500 font-semibold">
-                          De-active
-                        </p>
+                      </div>
+                      |
+                      <div>
+                        {item.Status === 1 ? (
+                          <button
+                            class="group relative z-0 h-10 overflow-hidden overflow-x-hidden rounded-md bg-neutral-950 px-6 py-2 text-neutral-50"
+                            type="button"
+                            onClick={() => handleDeactive(item)}
+                          >
+                            <span class="relative z-10">Deactive</span>
+                            <span class="absolute inset-0 overflow-hidden rounded-md">
+                              <span class="absolute left-0 aspect-square w-full origin-center translate-x-full rounded-full bg-blue-500 transition-all duration-500 group-hover:-translate-x-0 group-hover:scale-150"></span>
+                            </span>
+                          </button>
+                        ) : (
+                          <button
+                            class="group relative z-0 h-10 overflow-hidden overflow-x-hidden rounded-md bg-neutral-950 px-8 py-2 text-neutral-50"
+                            type="button"
+                            onClick={() => handleActive(item)}
+                          >
+                            <span class="relative z-10">Active</span>
+                            <span class="absolute inset-0 overflow-hidden rounded-md">
+                              <span class="absolute left-0 aspect-square w-full origin-center translate-x-full rounded-full bg-blue-500 transition-all duration-500 group-hover:-translate-x-0 group-hover:scale-150"></span>
+                            </span>
+                          </button>
+                        )}
                       </div>
                     </td>
                   </tr>
