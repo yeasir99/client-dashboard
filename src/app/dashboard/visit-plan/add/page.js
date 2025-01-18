74601e24fea6cfef18e0d9634101280c8621dd92
Yeasir Ahamed <yahamed007@gmail.com>
2025-01-19 01:44:29 +0600

    update visit plan

--- src/app/dashboard/visit-plan/add/page.js
+++ src/app/dashboard/visit-plan/add/page.js
@@ -1,15 +1,15 @@
-'use client'
-import { useSession } from "next-auth/react"
-import {useState, useEffect} from 'react'
+'use client';
+import { useSession } from 'next-auth/react';
+import { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import useGetData from "@/utils/useGetData";
-import axios from "axios";
+import useGetData from '@/utils/useGetData';
+import axios from 'axios';
 import { useRouter } from 'next/navigation';
 
 const page = () => {
-  const [name, setName] = useState('')
-  const [intName ,setIntName] = useState([])
+  const [name, setName] = useState('');
+  const [intName, setIntName] = useState([]);
   const [formData, setFormData] = useState({
     VisitPlanNo: '',
     VisitPlanDate: new Date().toISOString().split('T')[0],
@@ -17,63 +17,72 @@ const page = () => {
     InstitutionID: '',
     PurposeID: '',
     VisitUserID: '',
-    UserID: ''
-  })
-  const { data: session, status } = useSession()
-  useEffect(()=>{
-    if(session?.user){
+    UserID: '',
+  });
+  const { data: session, status } = useSession();
+  useEffect(() => {
+    if (session?.user) {
       setFormData({
         ...formData,
         VisitUserID: session.user.id,
-        UserID: session.user.id
-      })
-      setName(session.user.name)
+        UserID: session.user.id,
+      });
+      setName(session.user.name);
     }
+  }, [session]);
 
-  }, [session])
-
-const instutionType = useGetData('https://kblsf.site/DLogicKBL/salesforce_api.php?action=get_institutiontypes')
-const purposeCategory = useGetData('https://kblsf.site/DLogicKBL/salesforce_api.php?action=get_visitpurposes')
-
-const getPerpouse = async () => {
-  const res = await axios.post('https://kblsf.site/DLogicKBL/salesforce_api.php?action=generate_new_visit_plan_number')
-  setFormData({
-    ...formData,
-    VisitPlanNo: res.data.newVisitPlanNo
-  })
-}
-
-useEffect(()=>{
-  getPerpouse()
-}, [])
-
-const getInstutionName = async id => {
-  const res = await axios.get(
-    `https://kblsf.site/DLogicKBL/salesforce_api.php?action=get_institutionName&InstitutionTypeID=${id}`
+  const instutionType = useGetData(
+    'https://kblsf.site/DLogicKBL/salesforce_api.php?action=get_institutiontypes'
   );
-  if (res.data.length) {
-    setIntName(res.data);
-  }
-};
+  const purposeCategory = useGetData(
+    'https://kblsf.site/DLogicKBL/salesforce_api.php?action=get_visitpurposes'
+  );
 
-useEffect(() => {
-  if (formData.InstitutionTypeID) {
-    getInstutionName(formData.InstitutionTypeID);
-  }
-}, [formData.InstitutionTypeID]);
+  const getPerpouse = async () => {
+    const res = await axios.post(
+      'https://kblsf.site/DLogicKBL/salesforce_api.php?action=generate_new_visit_plan_number'
+    );
+    setFormData({
+      ...formData,
+      VisitPlanNo: res.data.newVisitPlanNo,
+    });
+  };
 
-const handleChange = e => {
-  setFormData({
-    ...formData,
-    [e.target.name]: e.target.value
-  })
-}
-const router = useRouter()
-const handleSubmit = async e =>{
-  e.preventDefault();
-  const res = await axios.post('https://kblsf.site/DLogicKBL/salesforce_api.php?action=create_visit_plan', formData)
-  router.push('/dashboard/visit-plan')
-}
+  useEffect(() => {
+    getPerpouse();
+  }, []);
+
+  const getInstutionName = async id => {
+    const res = await axios.get(
+      `https://kblsf.site/DLogicKBL/salesforce_api.php?action=get_institutionName&InstitutionTypeID=${id}`
+    );
+    if (res.data.length) {
+      setIntName(res.data);
+    }
+  };
+
+  useEffect(() => {
+    if (formData.InstitutionTypeID) {
+      getInstutionName(formData.InstitutionTypeID);
+    }
+  }, [formData.InstitutionTypeID]);
+
+  const handleChange = e => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const router = useRouter();
+  const handleSubmit = async e => {
+    e.preventDefault();
+    console.log(formData);
+    const res = await axios.post(
+      'https://kblsf.site/DLogicKBL/salesforce_api.php?action=create_visit_plan',
+      formData
+    );
+    router.push('/dashboard/visit-plan');
+  };
 
   return (
     <>
@@ -93,17 +102,17 @@ const handleSubmit = async e =>{
           Add New visit plan Requisition
         </h2>
         <form onSubmit={handleSubmit}>
-        <div>
+          <div>
             <label className="capitalize flex font-semibold text-md py-1">
               Visit No:
             </label>
 
             <input
-            type="text"
-            className="text-md outline-1 border-1 focus:ring-0 rounded-md w-full text-sm"
-            value={formData.VisitPlanNo}
-            readOnly
-          />
+              type="text"
+              className="text-md outline-1 border-1 focus:ring-0 rounded-md w-full text-sm"
+              value={formData.VisitPlanNo}
+              readOnly
+            />
           </div>
           <div>
             <label className="capitalize flex font-semibold text-md py-1">
@@ -111,76 +120,97 @@ const handleSubmit = async e =>{
             </label>
 
             <input
-            type="text"
-            className="text-md outline-1 border-1 focus:ring-0 rounded-md w-full text-sm"
-            value={name}
-            readOnly
-          />
+              type="text"
+              className="text-md outline-1 border-1 focus:ring-0 rounded-md w-full text-sm"
+              value={name}
+              readOnly
+            />
           </div>
           <div className="w-full">
-                    <label className="capitalize flex font-semibold text-md py-1">
-                      Visit Date:
-                    </label>
-          
-                    <DatePicker
-                      selected={formData.VisitPlanDate}
-                      onChange={date =>
-                        setFormData({
-                          ...formData,
-                          VisitPlanDate: date.toISOString().split('T')[0],
-                        })
-                      }
-                      className="rounded-md"
-                    />
-                  </div>
-          <div>
             <label className="capitalize flex font-semibold text-md py-1">
-            Institution/Party Type:
+              Visit Date:
             </label>
 
-            <select name="InstitutionTypeID" className="w-full rounded-md" value={formData.InstitutionTypeID} onChange={handleChange}>
+            <DatePicker
+              selected={formData.VisitPlanDate}
+              onChange={date =>
+                setFormData({
+                  ...formData,
+                  VisitPlanDate: date.toISOString().split('T')[0],
+                })
+              }
+              className="rounded-md"
+            />
+          </div>
+          <div>
+            <label className="capitalize flex font-semibold text-md py-1">
+              Institution/Party Type:
+            </label>
+
+            <select
+              name="InstitutionTypeID"
+              className="w-full rounded-md"
+              value={formData.InstitutionTypeID}
+              onChange={handleChange}
+            >
               <option value="" disabled={true}></option>
-              {instutionType.data.length && instutionType.data.map(item =>{
-               return <option value={item.ID} key={item.ID}>{item.CategoryName}</option>
-              })}
+              {instutionType.data.length &&
+                instutionType.data.map(item => {
+                  return (
+                    <option value={item.ID} key={item.ID}>
+                      {item.CategoryName}
+                    </option>
+                  );
+                })}
             </select>
           </div>
           <div>
-          <label className="capitalize flex font-semibold text-md py-1">
-          Institution/Party Name:
-          </label>
-
-          <select
-            name="InstitutionID"
-            className="w-full rounded-md"
-            onChange={handleChange}
-            value={formData.InstitutionID}
-            required
-            disabled={formData.InstitutionTypeID ? false : true}
-          >
-            <option value="" disabled={true}></option>
-            {intName.length &&
-              intName.map(item => (
-                <option value={item.InstitutionID} key={item.InstitutionID}>
-                  {item.InstitutionName}
-                </option>
-              ))}
-          </select>
-        </div>
-          <div>
             <label className="capitalize flex font-semibold text-md py-1">
-            Visit Purpose:
+              Institution/Party Name:
             </label>
 
-            <select name="PurposeID" className="w-full rounded-md" value={formData.PurposeID} onChange={handleChange}>
+            <select
+              name="InstitutionID"
+              className="w-full rounded-md"
+              onChange={handleChange}
+              value={formData.InstitutionID}
+              required
+              disabled={formData.InstitutionTypeID ? false : true}
+            >
               <option value="" disabled={true}></option>
-              {purposeCategory.data.length && purposeCategory.data.map(item =>(
-                <option value={item.ID} key={item.ID}>{item.CategoryName}</option>
-              ))}
+              {intName.length &&
+                intName.map(item => (
+                  <option value={item.InstitutionID} key={item.InstitutionID}>
+                    {item.InstitutionName}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <div>
+            <label className="capitalize flex font-semibold text-md py-1">
+              Visit Purpose:
+            </label>
+
+            <select
+              name="PurposeID"
+              className="w-full rounded-md"
+              value={formData.PurposeID}
+              onChange={handleChange}
+            >
+              <option value="" disabled={true}></option>
+              {purposeCategory.data.length &&
+                purposeCategory.data.map(item => (
+                  <option value={item.ID} key={item.ID}>
+                    {item.CategoryName}
+                  </option>
+                ))}
             </select>
           </div>
           <div className="mt-5">
-            <button className="capitalize bg-primary px-5 py-1 text-white rounded-md" type="submit">
+            <button
+              className="capitalize bg-primary px-5 py-1 text-white rounded-md"
+              type="submit"
+            >
               save visit plan
             </button>
           </div>
--- src/app/dashboard/visit-plan/view/[id]/page.js
+++ src/app/dashboard/visit-plan/view/[id]/page.js
@@ -1,35 +1,43 @@
-'use client'
-import axios from 'axios'
-import {useState, useEffect} from 'react'
+'use client';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
-const page = ({params}) => {
-    const [visitData, setVisitData] = useState({
-        status: 'pending',
-        data: null
-    })
+const page = ({ params }) => {
+  const [visitData, setVisitData] = useState({
+    status: 'pending',
+    data: null,
+  });
 
-    console.log(visitData)
+  console.log(visitData);
 
-    const getData = async id =>{
-        const res = await axios.get(`https://kblsf.site/DLogicKBL/salesforce_api.php?action=get_visit_plan&VisitPlanID=${id}`)
-        console.log(res)
-        setVisitData({
-            status: 'idle',
-            data: res.data?.VisitPlan ? res.data.VisitPlan : null
-        })
+  const getData = async id => {
+    const res = await axios.get(
+      `https://kblsf.site/DLogicKBL/salesforce_api.php?action=get_visit_plan&VisitPlanID=${id}`
+    );
+    console.log(res);
+    setVisitData({
+      status: 'idle',
+      data: res.data?.VisitPlan ? res.data.VisitPlan : null,
+    });
+  };
+
+  useEffect(() => {
+    if (params.id) {
+      getData(params.id);
     }
-
-    useEffect(()=>{
-        if(params.id){
-            getData(params.id)
-        }
-    }, [params.id])
-    if(visitData.status === 'pending'){
-        return <div className='text-xl font-semibold text-center py-6'>Loading...</div>
-    }
-    if(visitData.data === null){
-        return <div className='text-xl font-semibold text-center py-6'>No Data To Display</div> 
-    }
+  }, [params.id]);
+  if (visitData.status === 'pending') {
+    return (
+      <div className="text-xl font-semibold text-center py-6">Loading...</div>
+    );
+  }
+  if (visitData.data === null) {
+    return (
+      <div className="text-xl font-semibold text-center py-6">
+        No Data To Display
+      </div>
+    );
+  }
   return (
     <div className="flex justify-center">
       <div className="min-w-[600px] rounded-md bg-gray-300 p-5">
@@ -52,9 +60,17 @@ const page = ({params}) => {
           <h1 className="text-lg">Visit User Name:</h1>
           <h1>{visitData.data.VisitUserName}</h1>
         </div>
+        <div className="flex items-center gap-2">
+          <h1 className="text-lg">Institution Type:</h1>
+          <h1>{visitData.data.InstitutionTypeName}</h1>
+        </div>
+        <div className="flex items-center gap-2">
+          <h1 className="text-lg">Institution Name:</h1>
+          <h1>{visitData.data.InstituteName}</h1>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
